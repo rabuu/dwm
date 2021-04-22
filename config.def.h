b8bdc2e8f7937c7f37ad8d3a6882a3dabeb1146e
rabuu <rabuu.mail@gmail.com>
2021-04-22 19:02:11 +0200

    Unpatch centeredmaster layout

--- config.def.h
+++ config.def.h
@@ -44,8 +44,6 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-	{ "|M|",      centeredmaster },
-	{ ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
@@ -131,8 +129,6 @@ static Key keys[] = {
 	{ MODKEY|ControlMask,             XK_t,      				  setlayout,      	{.v = &layouts[0]} 				 },
 	{ MODKEY|ControlMask,             XK_f,      				  setlayout,      	{.v = &layouts[1]} 				 },
 	{ MODKEY|ControlMask,             XK_m,      				  setlayout,      	{.v = &layouts[2]} 				 },
-	{ MODKEY|ControlMask,             XK_c, 				      setlayout,        {.v = &layouts[3]} 				 },
-	{ MODKEY|ControlMask,             XK_o,     				  setlayout,        {.v = &layouts[4]} 				 },
 	{ MODKEY|ControlMask|ShiftMask,   XK_space,                   togglefloating,   { 0 }                            },
 	{ MODKEY|ControlMask,             XK_Return,                  togglefullscr,    { 0 }                            },
 
