a5a83ac32d477cda5d746b71a1baebc4bea4f669
rabuu <rabuu.github@gmail.com>
2021-06-17 23:32:58 +0200

    Apply swallow patch

--- config.def.h
+++ config.def.h
@@ -3,6 +3,7 @@
 /* appearance */
 static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static unsigned int gappx     = 6;        /* gaps between windows */
@@ -30,8 +31,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance 	   title       tags mask     isfloating   monitor */
-	{ NULL, 	    NULL,       NULL, 	      0,            0,           0 },
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
